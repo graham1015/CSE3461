1146ca388d4c379613317e1203dbe28e4a2494ad
graham1015 <31997987+graham1015@users.noreply.github.com>
2019-05-31 16:46:34 -0400

    Add files via upload

---
+++ ftpc.py
@@ -0,0 +1,58 @@
+#Author: Gavin Graham
+
+#Client Program
+#Import Packages
+import sys
+import os
+import socket
+
+#Check to see if command line args are valid
+if len(sys.argv) != 4:
+	print('Invalid arguments. Use form: "ftpc.py <remote-IP> <remote-port> <local-file>"');
+
+#Set variable for port, host, and filename
+HOST = sys.argv[1];
+PORT = int(sys.argv[2]);
+FILENAME = sys.argv[3];
+
+
+cliSocket = socket.socket(socket.AF_INET, socket.SOCKSTREAM);
+
+
+cliSocket.connect(HOST, PORT);
+print ('Connected with ', Host, ' on port ', str(PORT));
+
+
+# Check if file exists
+if not os.path.exists(FILENAME):
+	print ('Error: Enter valid filename.');
+
+#get size of file
+size = os.path.getsize(FILENAME);
+
+#prep size and filename to be sent
+sizeBytes = size.to_bytes(4, byteorder = 'big');
+correctedFILENAME = FILENAME.rjust(20);  
+filenameBytes = correctedFILENAME.encode(encoding="ascii");
+
+#send size
+cliSocket.send(sizeBytes);
+print ('Size sent');
+
+#send filename
+cliSocket.send(filenameBytes);
+print ('Name sent');
+
+#open file
+file = open(FILENAME, 'rb');
+
+#Read data and send in 1000 byte packets
+data = file.read(1000);
+while data:
+	cliSocket.send(data); 
+	data = file.read(1000);
+
+#close file and connection
+file.close();
+cliSocket.close();
+print('File Successfully transfered. Connection closed.')
---
+++ ftps.py
@@ -0,0 +1,60 @@
+#Author: Gavin Graham
+
+#Server Program
+#Import Packages
+import sys
+import os
+import socket
+
+
+
+#Check to see if command line args are valid
+if len(sys.argv) != 2:
+	print('Invalid arguments. Use form: "Ftps.py <remote-port>"');
+
+#Set variable for port and host
+HOST = '';
+PORT = int(sys.argv[1]);
+
+
+
+#Open the socket and confirm when open
+servSocket = socket.socket(socket.AF_INET, socket.SOCKSTREAM);
+servSocket.bind(HOST, PORT);
+print("Server is ready");
+
+#Connect with client
+connSock, addr = servSocket.accept();
+print ('Connected with ', addr);
+
+#First 4 bytes indicate the file size
+byteSize = connSock.recv(4);
+size = int.from_bytes(byteSize, byteorder='big');
+print ('File size: ', size);
+
+#The following 20 bytes indicate file name
+nameByte= connSock.recv(20);
+name = nameByte.decode().lstrip();
+print ('File name: ', name);
+
+#create subDirectory if does not exist
+folder = "recv/";
+if not os.path.exists(folder):
+		os.mkdir(folder);
+fileName = folder+name;
+
+#Open file
+file = open(fileName, 'wb+');
+
+#Read packets 1000 bytes at a time until the entire message is read. Then write to new file.
+bytesRead = 0;
+while bytesRead < size:
+	data = connSock.recv(1000)
+	bytesRead += len(data)
+	file.write(data)
+
+
+#Close file and connection
+file.close();
+connSock.close();
+print('File Successfully transfered. Connection closed.')
