e68ea92d7e486577d19db925148ce4afd16d4a78
graham1015 <31997987+graham1015@users.noreply.github.com>
2019-05-31 18:17:33 -0400

    Update ftpc.py

--- Lab2/ftpc.py
+++ Lab2/ftpc.py
@@ -16,7 +16,7 @@ PORT = int(sys.argv[2]);
 FILENAME = sys.argv[3];
 
 
-cliSocket = socket(socket.AF_INET, socket.SOCK_STREAM);
+cliSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
 
 
 cliSocket.connect(HOST, PORT);
