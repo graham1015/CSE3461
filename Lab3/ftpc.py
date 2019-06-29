6333b93bc0411bba57f2540b7e01dfd6dd49a85e
graham1015 <graham.1015@osu.edu>
2019-06-28 20:33:58 -0400

    finished

--- Lab3/ftpc.py
+++ Lab3/ftpc.py
@@ -1,1 +1,74 @@
+#Author : Gavin Graham
 
+#Client Program
+#Import Packages
+import socket
+import sys
+import os
+import time
+
+#Check to see if command line args are valid
+if len(sys.argv) != 5:
+	print('Invalid arguments. Use form: "ftpc.py <remote-IP> <remote-port> <troll-port> <local-file>"')
+
+HOST = sys.argv[1]
+PORT = int(sys.argv[2])
+TROLL = int(sys.argv[3])
+FILENAME = sys.argv[4]
+
+#Create connection
+try:
+	cliSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except (socket.error):
+	print('Failed to create socket.')
+	sys.exit();
+
+cliSocket.bind((socket.gethostname(),4567))
+
+print ('Socket Created') 
+
+# Check if file exists
+if not os.path.exists(FILENAME):
+	print ('Error: Enter valid filename.')
+
+#get size of file
+size = os.path.getsize(FILENAME)
+
+#prep size, filename, IP, and port to be sent
+sizeBytes = size.to_bytes(4, byteorder = 'big')
+correctedFILENAME = FILENAME.rjust(20)
+filenameBytes = correctedFILENAME.encode(encoding="ascii")
+hostBytes = socket.inet_aton(HOST) 
+portBytes = PORT.to_bytes(2, byteorder='big') 
+
+#open file to read binary
+file = open(FILENAME, 'rb') 
+
+#Set flags and send size and filename
+flag = 1	
+flagBytes = flag.to_bytes(1, byteorder = 'big') 
+
+cliSocket.sendto(hostBytes+portBytes+flagBytes+sizeBytes,('', TROLL))
+print ('Size sent')
+
+flag = 2
+flagBytes = flag.to_bytes(1, byteorder = 'big') 
+
+cliSocket.sendto(hostBytes+portBytes+flagBytes+filenameBytes,('', TROLL))
+print ('Name sent')
+
+flag = 3
+flagBytes = flag.to_bytes(1, byteorder = 'big') 
+
+#read and send data of file
+data = file.read(1000)
+while data:
+	cliSocket.sendto(hostBytes+portBytes+flagBytes+data,('', TROLL)) 
+	data = file.read(1000)
+	time.sleep(.01)
+cliSocket.sendto(hostBytes+portBytes+flagBytes+data,('', TROLL)) 
+
+file.close()
+cliSocket.close()
+
+print('File Successfully transfered. Connection closed.')
--- Lab3/ftps.py
+++ Lab3/ftps.py
@@ -1,1 +1,61 @@
+#Author : Gavin Graham
 
+
+#Server Program
+#Import Packages
+import sys
+import os
+import socket
+
+#Check to see if command line args are valid
+if len(sys.argv) != 2:
+	print('Invalid arguments. Use form: "ftps.py <remote-port>"');
+
+#Set variable for port and host
+HOST = socket.gethostname()
+PORT = int(sys.argv[1])
+
+try:
+	cliSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error:
+	print('Failed to create socket.')
+	sys.exit();
+
+print ('Socket Created') 
+
+try:
+	s.bind((HOST,PORT))
+	print ('Socket bind complete')
+except socket.error:
+	print('Failed to bind socket to host')
+	sys.exit()
+
+print("Server is ready")
+
+while True:
+	data, addr = cliSocket.recvfrom(1000)
+	print ('Recieving from' , addr)
+	clientIP_encoded = data[0:4] 
+	clientPORT_encoded = data[4:6] 
+	switch (int.from_bytes(data[6:7], byteorder='big')): 
+		case 1:
+			sizeByte = data[7:len(data)] 
+			size = int.from_bytes(byte_size, byteorder='big')
+			print ('File size: ', size)
+		case 2:
+			filenameBytes = data[7:len(data)]
+			filename = byte_filename.decode()
+			filename = filename.lstrip()
+			if not os.path.exists('recv/'):
+				os.makedirs('recv/')
+			file = open('recv/' + filename, 'wb')
+		case 3:
+			data = data[7:len(data)]
+			file.write(data)
+
+			if data == b"":
+				break
+
+file.close()
+cliSocket.close()
+print('File Successfully transfered. Connection closed.')
