c75f492223ab89133c2a40fba25181a86b1d4c62
graham1015 <graham.1015@osu.edu>
2019-07-18 23:50:47 -0400

    f

--- Lab4/ftps.py
+++ Lab4/ftps.py
@@ -9,7 +9,7 @@ import socket
 
 #Check to see if command line args are valid
 if len(sys.argv) != 3:
-	print('Invalid arguments. Use form: "ftps.py <remote-port>"')
+	print('Invalid arguments. Use form: "ftps.py <local-port-on-System-2> <troll-port-on-System-2>"')
 	sys.exit()
 
 #Set variable for port and host
@@ -41,7 +41,6 @@ while 1:
 	data, addr = servSocket.recvfrom(1000)
 	cliIP_encoded = data[0:4] 
 	cliPORT_encoded = data[4:6]
-	cliFLAG = int.from_bytes(data[6:7], byteorder='big') 
 	cliACK_encoded = data[7:8]
 	if data == dataDup:
 		servSocket.sendto(cliIP_encoded+cliPORT_encoded+cliACK_encoded, ("", TROLL))
@@ -50,21 +49,22 @@ while 1:
 		dataDup = data
 		servSocket.sendto(cliIP_encoded+cliPORT_encoded+cliACK_encoded, ("", TROLL))
 		if (int.from_bytes(data[6:7], byteorder='big') == 1): 
-			sizeByte = data[7:len(data)] 
+			sizeByte = data[8:len(data)] 
 			print ('Recieving from' , addr)
 			size = int.from_bytes(sizeByte, byteorder='big')
 			print ('File size: ', size)
 		if (int.from_bytes(data[6:7], byteorder='big') == 2): 
-			filenameBytes = data[7:len(data)]
+			filenameBytes = data[8:len(data)]
 			filename = filenameBytes.decode()
 			filename = filename.lstrip()
 			if not os.path.exists('recv/'):
 				os.makedirs('recv/')
 			file = open('recv/' + filename, 'wb')
+			print ('File name: ', filename)
 		if (int.from_bytes(data[6:7], byteorder='big') == 3): 
-			data = data[7:len(data)]
+			data = data[8:len(data)]
 			file.write(data)
-			if data == b"":
+			if len(data) < 1000:
 				break
 	
 file.close()
