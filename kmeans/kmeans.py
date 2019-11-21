c3f12dbd5e56f8d1ffa3d663dd4de1934ad4ef84
graham1015 <31997987+graham1015@users.noreply.github.com>
2019-11-21 06:53:45 -0500

    Update and rename kmeans to kmeans.py

---
+++ kmeans/kmeans.py
@@ -0,0 +1,17 @@
+#Author Gavin Graham
+
+
+databaseFile
+k
+maxIters
+eps
+outputFile
+
+def read_data
+
+def genKmeans (database, k, n, e output-file):
+
+  #randomly initiate k centroids
+  
+  #repeat until the new and old cluster centroids are e-difference or max iterations already
+  
